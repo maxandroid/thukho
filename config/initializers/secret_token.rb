c7126ed1679d07f5a1590eb4a71def1827de6f40
Max <sieu_ngoc_2007@yahoo.com>
2014-03-08 03:07:28 +0700

    init rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thukho::Application.config.secret_key_base = 'a12fdac41561bc6d8c863d8487c8ceebf2f6f43ba670ff3176a7b3724218ea7fc1006083cf97026fc532af232c6a01f6582f5d69f76db5cb7604f44f202c28ac'
